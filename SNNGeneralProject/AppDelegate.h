996eefb5124971e8eda27b888f53142fe0478e79
dzcx-shinana <nanaseeyou@hotmail.com>
2017-11-22 11:34:10 +0800

    Initial Commit

---
+++ SNNGeneralProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNNGeneralProject
+//
+//  Created by dzcx-shinana on 2017/11/22.
+//  Copyright © 2017年 dzcx-shinana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNNGeneralProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNNGeneralProject
+//
+//  Created by dzcx-shinana on 2017/11/22.
+//  Copyright © 2017年 dzcx-shinana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
